92b2034315f9dde5f1c60ee8530a1fdf14d67967
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-10-04 23:21:03 -0300

    Create container compoennt. Adjust routin module and app module with new route.

---
+++ src/app/app-routing-module/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { ListContainerComponent } from "../components/container/list-container/list-container.component";
+
+export const routes: Routes = [
+  { path: 'list', component: ListContainerComponent },
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AddressComponent } from './components/profile/address/address.component';
@@ -15,6 +14,8 @@ import { ExampleComponent } from './multi-directive/example/example.component';
 import { EMPLOYEE_LIST } from './services/employee-token';
 import { ArtEmployeeService } from './services/art-employee.service';
 import { ScienceEmployeeService } from './services/science-employee.service';
+import { ListContainerComponent } from './components/container/list-container/list-container.component';
+import { AppRoutingModuleModule } from './app-routing-module/app-routing-module.module';
 
 @NgModule({
   declarations: [
@@ -23,13 +24,14 @@ import { ScienceEmployeeService } from './services/science-employee.service';
     AddressComponent,
     ApplyServiceDirective,
     MultiDirectiveComponent,
-    ExampleComponent
+    ExampleComponent,
+    ListContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ReactiveFormsModule,
-    ReactiveFormModule
+    ReactiveFormModule,
+    AppRoutingModuleModule
   ],
   providers: [
     {
