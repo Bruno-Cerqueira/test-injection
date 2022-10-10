75783bf524b0bd0811d39fde833f1bdadf8a37fb
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-10-09 23:38:09 -0300

    Adjust counter routes. Add counter selectors and component in list container.

--- src/app/app-routing-module/routes.ts
+++ src/app/app-routing-module/routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { ListContainerComponent } from "../components/container/list-container/list-container.component";
 import { MultiDirectiveComponent } from "../multi-directive/multi-directive.component";
+import { CounterComponent } from "../store-data/components/counter/counter.component";
 import { AllowExitListGuard } from "./guards/allow-exit-list.guard";
 import { AllowListGuard } from "./guards/allow-list.guard";
 
 export const routes: Routes = [
   { path: 'list', component: ListContainerComponent, canActivate: [AllowListGuard], canDeactivate: [AllowExitListGuard] },
-  { path: '', component: MultiDirectiveComponent },
+  { path: 'counter-list', component: CounterComponent },
+  { path: '', redirectTo: 'counter-list', pathMatch: 'full' },
 ];
